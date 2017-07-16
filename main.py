fcd780500df0d23a3c8f676995daa1997b3eac80
Rorounifix <rorounifix@gmail.com>
2017-07-16 17:01:00 +0300

    Update UI

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from kivy.config import Config
 
+
 Config.set("graphics","width","500")
 Config.set("graphics","height","500")
 Config.set("graphics","borderless","0")
@@ -13,68 +14,62 @@ Config.set("graphics","borderless","0")
 Builder.load_string('''
 <Cstm@Button>
     font_size:"80dp"
-
+    
 
 <RootWidget>
     id:tictac
     rows: 5
     
+    Label:
+        text: 'Tic-Tac-Toe Game' 
+        
+    BoxLayout:
+        Cstm:
+            id: set_1
+            display: 1
+            on_press: self.text = root.whos_turn(self.display)
+        Cstm:
+            id: set_2
+            display: 2
+            on_press: self.text = root.whos_turn(self.display)
+        Cstm:
+            id: set_3
+            display: 3
+            on_press: self.text = root.whos_turn(self.display)
+    BoxLayout:
+        Cstm:
+            id: set_4
+            display: 4
+            on_press: self.text = root.whos_turn(self.display)
+        Cstm:
+            id: set_5
+            display: 5
+            on_press: self.text = root.whos_turn(self.display)
+        Cstm:
+            id: set_6
+            display: 6
+            on_press: self.text = root.whos_turn(self.display)
+    BoxLayout:
+        Cstm:
+            id: set_7
+            display: 7
+            on_press: self.text = root.whos_turn(self.display)
+        Cstm:
+            id: set_8
+            display: 8
+            on_press: self.text = root.whos_turn(self.display)
+        Cstm:
+            id: set_9
+            display: 9
+            on_press: self.text = root.whos_turn(self.display)
     BoxLayout:
         Label:
-            id:text_label
-            font_size:'40dp'
-            text: 'Tic-Tac-Toe Games' 
-    BoxLayout:
-        Cstm:
-            id:text_1
-            text: ''
-            on_press: text_1.text = "X"
-        Cstm:
-            id:text_2
-            text: ''
-            on_press: text_2.text = "X"
-        Cstm:
-            id:text_3
-            text: ''
-            on_press: text_3.text = "X"
-
-    BoxLayout:
-        Cstm:
-            id:text_4
-            text: ''
-            on_press: text_4.text = "X"
-        Cstm:
-            id:text_5
-            text: ''
-            on_press: text_5.text = "X"
-        Cstm:
-            id:text_6
-            text: ''
-            on_press: text_6.text = "X"
-    BoxLayout:
-        Cstm:
-            id:text_7
-            text: ''
-            on_press: text_7.text = "X"
-        Cstm:
-            id:text_8
-            text: ''
-            on_press: text_8.text = "X"
-        Cstm:
-            id:text_9
-            text: ''
-            on_press: text_9.text = "X"
-
-    BoxLayout:
-        Label:
-            font_size:'30dp'
-            text: "O : 10"
+            text: 'O = 1' 
         Button:
-            font_size:'30dp'
-            text: "Start"
+            text: 'Start Game'
+            on_press: #root.start_game()
         Label:
-            font_size:'30dp'
-            text: "X : 10"
+            text: 'X = 5'
         
 
 
@@ -82,7 +77,44 @@ Builder.load_string('''
 
 
 class RootWidget(GridLayout):
-    pass
+    
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+        self.turn = None # x or o
+        self.color = None
+
+    
+
+    def cpu_brain(self):
+        pass
+
+    def start_game(self):
+        root_widget = self.ids['set_1']
+        root_widget.add_widget()
+
+    def whos_turn(self, btn_id):
+        set_id = 'set_{}'.format(btn_id)
+        button = self.ids[set_id]
+        button.disabled = True
+        
+        
+        if self.turn == 'x' or self.turn == '':
+            self.turn = 'o'
+            self.color = (0,9,0,1)
+        else:
+            self.turn = 'x'
+            self.color = (9,0,0,1)
+        button.background_color = self.color   
+        return self.turn
+                
+        
+        
+        
+            
+        
+  
+        
+        
 
 
 
